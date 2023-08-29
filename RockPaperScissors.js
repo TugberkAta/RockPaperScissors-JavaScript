d2cb9895483c933f4d7eb1a8275d53f30df5a732
TugberkAta <atatugberk@yahoo.com>
2023-08-29 14:10:33 +0300

    Add the main game loop

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,4 +1,5 @@
 let computerChoice;
+let playerChoice;
 
 function getRandomInt(number){
     return Math.floor(Math.random()*number)+1;
@@ -15,8 +16,29 @@ function getComputerChoice(){
     else if(randomNumber ===3){
         computerChoice = "Scissors"
     }
-    console.log(computerChoice)
 }
 
 getComputerChoice()
+playerChoice = prompt("Choose between Rock, Paper or Scissors")
 
+if(computerChoice === playerChoice){
+    console.log("Its a draw");
+}
+
+else if(computerChoice === "Rock" && playerChoice === "Scissors"){
+    console.log("You, lose");
+}
+
+else if(computerChoice === "Paper" && playerChoice === "Rock"){
+    console.log("You, lose");
+}
+
+else if(computerChoice === "Scissors" && playerChoice === "Paper"){
+    console.log("You, lose");
+}
+
+else{
+    console.log("You won!!");
+}
+
+console.log("Computer Picked: " + computerChoice)
