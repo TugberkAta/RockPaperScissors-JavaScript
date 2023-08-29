deedfd350fab88d26c32f8ae070e346b399bb3ea
TugberkAta <atatugberk@yahoo.com>
2023-08-29 14:47:45 +0300

    Add function to capitalise letters
    Make the user input not case sensitive
    
    Diasable the user from entering any input not intended

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -8,32 +8,42 @@ function getRandomInt(number){
 function getComputerChoice(){
     let randomNumber = getRandomInt(3);
     if (randomNumber === 1) {
-        computerChoice = "Rock"
+        computerChoice = "rock"
     }
     else if(randomNumber === 2) {
-        computerChoice = "Paper"
+        computerChoice = "paper"
     }
     else if(randomNumber ===3){
-        computerChoice = "Scissors"
+        computerChoice = "scissors"
     }
+}
+
+function capitalize(string){
+   return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
 getComputerChoice()
-playerChoice = prompt("Choose between Rock, Paper or Scissors")
 
-if(computerChoice === playerChoice){
+playerChoice = prompt("Choose between Rock, Paper or Scissors").toLowerCase()
+
+while(playerChoice !== "rock" && playerChoice !== "scissors" && playerChoice !== "paper"){
+    console.log("Please Enter a Correct Input");
+    playerChoice = prompt("Choose between Rock, Paper or Scissors").toLowerCase()
+}
+
+ if(computerChoice === playerChoice){
     console.log("Its a draw");
 }
 
-else if(computerChoice === "Rock" && playerChoice === "Scissors"){
+else if(computerChoice === "rock" && playerChoice === "scissors"){
     console.log("You, lose");
 }
 
-else if(computerChoice === "Paper" && playerChoice === "Rock"){
+else if(computerChoice === "paper" && playerChoice === "rock"){
     console.log("You, lose");
 }
 
-else if(computerChoice === "Scissors" && playerChoice === "Paper"){
+else if(computerChoice === "scissors" && playerChoice === "paper"){
     console.log("You, lose");
 }
 
@@ -41,4 +51,4 @@ else{
     console.log("You won!!");
 }
 
-console.log("Computer Picked: " + computerChoice)
+console.log("Computer Picked: " + capitalize(computerChoice))
